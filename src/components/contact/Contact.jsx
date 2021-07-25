4e55e162927a026bdc0d757f9c12150a3ddbb303
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-07-25 23:06:22 +0530

    scss

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,7 @@
+// import React from "react"; // After react 17 you don't import react from 'react';
+
+import "./Contact.scss";
+
+export default function Contact() {
+  return <div className="contact"></div>;
+}
